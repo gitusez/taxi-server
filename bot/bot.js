858bd752ec6efb5635c986b2d151a58da472b2b9
gitusez <rob1zshirenyan@gmail.com>
2025-04-19 18:42:51 +0300

    add find .env bot..js

--- bot/bot.js
+++ bot/bot.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" }); // ← добавили
 const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.BOT_TOKEN;
